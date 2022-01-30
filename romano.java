c25b44f50b41abb47efdb5c6a7a68989a7bc8b37
Euler G Gonçalves <goncalves.euler@gmail.com>
2022-01-30 11:25:45 -0300

    Update romano.java
    versao 10

--- romano.java
+++ romano.java
@@ -1,37 +1,65 @@
 
-
 import java.util.Scanner;
 
 class romano{
+    private static int in;
+    private static int numAlfa;
+
     public static void main(String args[]){
-        //System.out.println("Hello Roman");
 
+        do {
+            System.out.print("Entrada (1 a 4999): "); //Arabico
+            Scanner in = new Scanner( System.in );
+            int numAlfa = in.nextInt();
 
-        System.out.print("Entrada: ");
-        Scanner sc = new Scanner(System.in);
-        String entrada = sc.nextLine();
-        //System.out.print(entrada)
-;
-        System.out.println();
-        System.out.print("saida: ");
-        /*
-        I - 1
-        V - 5
-        X - 10
-        C - 100
-        M - 1000
-        */
+            //System.out.println( in );
+            System.out.println( numAlfa );
+        } while ( numAlfa > 4999 );
+
         
-        String [] array = entrada.split("");
-        for (int k=0; k < entrada.length(); k++){
-            if (array[k].equals("1")) {
-                array[k] = "I";
-            } 
-            System.out.print(array[k]);
-        }
+        
+        int unidade = numAlfa % 10;
+        int dezena = numAlfa % 100;
+        int centena = numAlfa % 1000;
+        int milhar = numAlfa % 10000;
+        System.out.println(unidade);
+        System.out.println(dezena);
+        System.out.println(centena);
+        System.out.println(milhar);
+
+
+        System.out.println();
+        System.out.print("saida: "); // Romano
+
+
+
+        //String[] StrArrayNumRomano = new String[20];
+        
+        
+        // unidade
+        /*
+        if (resto == 1) { StrArrayNumRomano[k] = "I"; }
+        if (resto == 2) { StrArrayNumRomano[k] = "II"; }
+        if (resto == 3) { StrArrayNumRomano[k] = "III"; }
+        if (resto == 4) { StrArrayNumRomano[k] = "IV"; }
+        if (resto == 5) { StrArrayNumRomano[k] = "V"; }
+        if (resto == 6) { StrArrayNumRomano[k] = "VI"; }
+        if (resto == 7) { StrArrayNumRomano[k] = "VII"; }
+        if (resto == 8) { StrArrayNumRomano[k] = "VIII"; }
+        if (resto == 9) { StrArrayNumRomano[k] = "IX"; }
+        System.out.print(" " + StrArrayNumRomano[k]);
+        */
+
+
+        // dezena
+
+
+
+        // centena
+
+
+        // milhar
 
 
     }
 }
-
-
